8c11fd282bd060faa813de8150ac55f1c9635c9c
kill_your_soul <platonovdi@gmail.com>
2024-05-06 16:00:02 +0300

    Init repo

---
+++ core/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+    PROJECT_NAME: str
+    API_V1_STR: str = "/api/v1"
+    VERSION: str
+
+
+settings = Settings()
---
+++ core/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+from core.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
